00e1a0dd5d85c69b0257dc9f2411ddcf076f0ce8
Sunil Gupta <ssunilgupta22@yahoo.co.in>
2022-04-17 02:19:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var http = require("http");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Heroku world!");
+  response.write("Hello world !");
+  response.write("This is Sunil world !");
   response.end();
 }).listen(process.env.PORT);
